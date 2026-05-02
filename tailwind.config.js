cb447778e6b02314f65e3df22fa6a82e4c560ba0
emily535 <emily@conciergeforrealtors.com>
2026-05-02 16:18:24 -0400

    Create tailwind.config.js with Ashley Smith's color palette

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        softIvory: '#F6F5F3', // soft ivory
+        warmWhite: '#FAF8F0', // warm white
+        deepNavy: '#002147', // deep navy
+        dustyBlushPink: '#EAB8C8', // dusty blush pink
+        champagneGold: '#F7E7CE', // champagne gold
+        mutedTaupe: '#C8BDA2', // muted taupe
+        softSage: '#C9D6D5' // soft sage
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
